0dfe1cb46a5923a7d3a23c5154bc4630ea690d9f
feraxhp <feraxhp+gh@gmail.com>
2024-09-08 07:20:42 -0500

    + create repo gitea

--- src/girep/repos/gitea/implementation.rs
+++ src/girep/repos/gitea/implementation.rs
@@ -5,7 +5,7 @@ use crate::animations;
 use crate::girep::base::Platform;
 use crate::girep::config::Config;
 use crate::girep::repo::Repo;
-use crate::girep::repos::gitea::errors::error_mannager;
+use crate::girep::repos::gitea::errors::{error_mannager, DebugData};
 use async_trait::async_trait;
 use color_print::cprintln;
 use hyper::HeaderMap;
@@ -62,7 +62,11 @@ impl Platform for Gitea {
 
         let response_text = error_mannager(
                 result,
-                owner,
+                DebugData{
+                    rtype: crate::girep::repos::gitea::errors::Rtype::List,
+                    owner: owner.clone(),
+                    repo: None,
+                },
                 self.config.clone(),
                 "Failed to fetch repositories".to_string(),
                 |str| { load_animation.finish_with_error(str); }
@@ -97,84 +101,60 @@ impl Platform for Gitea {
     }
 
     async fn create_repo(&self, owner: String, repo: Repo) -> Repo {
-        todo!()
-        // let client = reqwest::Client::new();
+        let client = reqwest::Client::new();
 
-        // let load_animation = animations::creation::Create::new("Creating repository ...");
+        let load_animation = animations::creation::Create::new("Creating repository ...");
 
-        // let result = client
-        //     .post(format!("https://{}/api/v1/user/repos", self.config.endpoint))
-        //     .headers(self.header.clone())
-        //     .json(&serde_json::json!({
-        //         "name": repo.full_name,
-        //         "description": repo.description,
-        //         "private": repo.state == "private",
-        //     }))
-        //     .send()
-        //     .await
-        //     .unwrap_or_else(
-        //         |e| {
-        //             load_animation.finish_with_error("Failed to contact the platform");
-        //             eprintln!("Failed to create repository: {}", e);
-        //             cprintln!("<y>Unknown error</>");
-        //             exit(101);
-        //         }
-        //     );
+        let result = client
+            .post(format!("https://{}/api/v1/user/repos", self.config.endpoint))
+            .headers(self.header.clone())
+            .header("content-type", "application/json")
+            .json(&serde_json::json!({
+                "name": repo.full_name,
+                "description": repo.description,
+                "private": repo.state == "private",
+            }))
+            .send()
+            .await
+            .unwrap_or_else(
+                |e| {
+                    load_animation.finish_with_error("Failed to contact the platform");
+                    eprintln!("Failed to create repository: {}", e);
+                    cprintln!("<y>Unknown error</>");
+                    exit(101);
+                }
+            );
 
-        // let status = result.status();
+        let response_text = error_mannager(
+                result,
+                DebugData{
+                    rtype: crate::girep::repos::gitea::errors::Rtype::Create,
+                    owner: owner.clone(),
+                    repo: Some(repo.full_name.clone()),
+                },
+                self.config.clone(),
+                "Failed to create repository".to_string(),
+                |str| { load_animation.finish_with_error(str); }
+            ).await;
 
-        // let response_text = result.text().await.unwrap_or_else(|e| {
-        //     load_animation.finish_with_error("Failed to create repository");
-        //     eprintln!("Failed to read the response text: {}", e);
-        //     cprintln!("<y>Unknown error</>");
-        //     exit(101);
-        // });
+        let transpiler: Transpiler = serde_json::from_str(&response_text).unwrap_or_else(
+            |e| {
+                load_animation.finish_with_error("Failed to create repository");
+                eprintln!("Failed to parse the response: {}", e);
+                eprintln!("Response: {}", response_text);
+                cprintln!("<y>Unknown error</>");
+                exit(101);
+            }
+        );
 
-        // let repo: Repo = match status.as_u16() {
-        //     201 => {
-        //         let transpiler: Transpiler = match serde_json::from_str(&response_text) {
-        //             Ok(transpilers) => transpilers,
-        //             Err(_) => {
-        //                 let error: Error = match serde_json::from_str(&response_text) {
-        //                     Ok(error) => error,
-        //                     Err(_) => {
-        //                         Error {
-        //                             message: format!("{}", response_text)
-        //                         }
-        //                     }
-        //                 };
-        //                 load_animation.finish_with_error("Failed to create repository");
-        //                 eprintln!("* Please check your token.");
-        //                 eprintln!("  Pconf name: {}", &self.config.pconf.clone());
-        //                 eprintln!("  User: {}", owner);
-        //                 exit(101);
-        //             }
-        //         };
-        //         Repo {
-        //             full_name: transpiler.full_name,
-        //             description: transpiler.description,
-        //             state: if transpiler.private { "private".to_string() } else { "public".to_string() },
-        //             html_url: transpiler.html_url,
-        //             clone_url: transpiler.clone_url,
-        //         }
-        //     },
-        //     400 => {
-        //         let error: Error = match serde_json::from_str(&response_text) {
-        //             Ok(error) => error,
-        //             Err(_) => {
-        //                 Error {
-        //                     message: format!("{}", response_text)
-        //                 }
-        //             }
-        //         };
-        //         load_animation.finish_with_error("Failed to create repository");
-               
-        //     },
-        //     _ => {
-        //         load_animation.finish_with_error("Failed to create repository");
-                
-        //     }
-        // };
+        load_animation.finish_with_success("Done!");
 
+        Repo {
+            full_name: transpiler.full_name,
+            description: transpiler.description,
+            state: if transpiler.private { "private".to_string() } else { "public".to_string() },
+            html_url: transpiler.html_url,
+            clone_url: transpiler.clone_url,
+        }
     }
 }
