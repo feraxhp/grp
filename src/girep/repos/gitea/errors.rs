7913bd8d5755c8d5eac3081ba559353841417029
feraxhp <feraxhp+gh@gmail.com>
2024-09-08 07:20:42 -0500

    + create repo gitea

--- src/girep/repos/gitea/errors.rs
+++ src/girep/repos/gitea/errors.rs
@@ -1,17 +1,37 @@
-use std::process::exit;
+use crate::girep::config::Config;
 use color_print::cprintln;
 use reqwest::Response;
 use serde::Deserialize;
-use crate::girep::config::Config;
+use std::process::exit;
 
 #[derive(Deserialize)]
 struct Error {
     message: String,
 }
 
+pub struct DebugData {
+    pub rtype: Rtype,
+    pub owner: String,
+    pub repo: Option<String>,
+}
+
+pub enum Rtype {
+    List,
+    Create,
+}
+
+impl Rtype {
+    pub fn to_string(&self) -> String {
+        match self {
+            Rtype::List => "List".to_string(),
+            Rtype::Create => "Create".to_string(),
+        }
+    }
+}
+
 pub(crate) async fn error_mannager(
     result: Response,
-    owner: String,
+    debug_data: DebugData,
     config: Config,
     base_message: String,
     finish_animation: impl Fn(&str),
@@ -26,7 +46,12 @@ pub(crate) async fn error_mannager(
     });
 
     match status.as_u16() {
-        200 => { return text; },
+        200 if matches!(debug_data.rtype, Rtype::List) => { return text; },
+        201 if matches!(debug_data.rtype, Rtype::Create) => { return text; },
+        409 if matches!(debug_data.rtype, Rtype::Create) => {
+            finish_animation("Repository already exists");
+            cprintln!("Repository: <m>({}/{})</>", debug_data.owner, debug_data.repo.clone().unwrap());
+        },
         _ => {
             let error: Error = serde_json::from_str(&text)
                 .unwrap_or_else(|_| {
@@ -41,15 +66,27 @@ pub(crate) async fn error_mannager(
                     finish_animation("Bad credentials");
                     eprintln!("* Please check your token.");
                     eprintln!("  Pconf name: {}", config.pconf.clone());
-                    eprintln!("  User: {}", owner);
+                    eprintln!("  User: {}", debug_data.owner);
                 },
                 _ if error.message.starts_with("user redirect does not exist [name: ") => {
                     finish_animation("User/org does not exist");
-                    cprintln!("User/org: <m>({})</>", owner);
+                    cprintln!("User/org: <m>({})</>", debug_data.owner);
+                },
+                _ if error.message.starts_with("token does not have at least one of required scope(s):") => {
+                    finish_animation("Bad token scope");
+                    cprintln!("\n<g>* Pconf name: <s,i>{}</>", config.pconf.clone());
+                    cprintln!("<i>* Please check your token.</>");
+                    cprintln!("  - You need to add one of the following scopes:");
+                    let scopes_index = (error.message.find("[").unwrap() + 1) .. error.message.find("]").unwrap();
+                    let scopes = error.message[scopes_index].split(",");
+                    for scope in scopes.enumerate() {
+                        cprintln!("    <#e3750e>{}. <m>{}</>", scope.0 + 1, scope.1);
+                    }
                 },
                 _ => {
                     finish_animation(base_message.as_str());
-                    println!("{}", &text);
+                    eprintln!("{}", &text);
+                    cprintln!("Request type: <r>{}</>", debug_data.rtype.to_string());
                     cprintln!("<y>Unknown error</> {}", status.as_u16());
                 }
             }
