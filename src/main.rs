a2d93c6763ceca9ca46a4c993bff087713177491
feraxhp <feraxhp+gh@gmail.com>
2024-09-16 15:05:15 -0500

    Build improvements

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ mod config;
 mod macros;
 mod animations;
 
+use std::io;
+use std::io::Write;
+use std::process::exit;
 use crate::config::command::{config_command, config_manager};
 use crate::girep::commands::create::mannager::create_mannager;
 use crate::girep::commands::create::subcommand::create_subcommand;
 use crate::girep::commands::list::mannager::list_manager;
 use crate::girep::commands::list::subcommand::list_subcommand;
 use crate::macros::macros::invalid;
-use clap::command;
+use clap::{arg, command, crate_version, ArgAction};
 use color_print::cprintln;
 use crate::girep::commands::delete::mannager::delete_manager;
 use crate::girep::commands::delete::subcommand::delete_subcommand;
@@ -22,6 +25,7 @@ async fn main() {
     let commands = command!()
         .name("grp")
         .about("A simple CLI to manage platforms for git repositories")
+        .arg(arg!(-v --vnumber "Prints the version number to the standar output").exclusive(true))
         .subcommand(config_command())
         .subcommand(list_subcommand())
         .subcommand(create_subcommand())
@@ -30,6 +34,16 @@ async fn main() {
 
     let user_settings = config::loader::load_configurations();
 
+    match commands.clone().args_present() {
+        true => {
+            if *commands.get_one::<bool>("vnumber").unwrap_or(&false) {
+                let version = crate_version!();
+                let _ = io::stdout().write(version.as_bytes());
+                exit(0);
+            }
+        },
+        _ => {}
+    }
     match commands.subcommand() {
         Some(sub) => match sub {
             ("config", config) => config_manager(config),
