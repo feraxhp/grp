ee0759a7894edf35316aacecead120e0d06d6174
feraxhp <feraxhp+gh@gmail.com>
2024-10-02 04:15:28 +0000

    * create optimizations * repo print improvement

--- src/girep/repo.rs
+++ src/girep/repo.rs
@@ -1,6 +1,8 @@
 // Copyright 2024 feraxhp
 // Licensed under the MIT License;
 
+use std::cmp::max;
+
 pub(crate) struct Repo {
     pub(crate) full_name: String,
     pub(crate) description: String, // optional
@@ -24,29 +26,35 @@ impl Repo {
 #[macro_export]
 macro_rules! show {
     ($r:expr) => {
+        use std::cmp::max;
 
         if $r.is_empty() {
             eprintln!("No repositories found");
             return;
         }
 
-        let max_name = $r.iter().map(|r| r.full_name.len()).max().unwrap();
         let length = $r.len().to_string().len();
+        let max_name = $r.iter().map(|r| r.full_name.len()).max().unwrap();
+        let max_state = $r.iter().map(|r| r.state.len()).max().unwrap();
+
+        let max_name = max(4, max_name);
+        let max_state = max(5, max_state);
 
         eprintln!(
-            " {0:#^dig$} | {1: <width$} | {2: <7} | {3}",
+            " {0:#^dig$} | {1: <width$} | {2: <state$} | {3}",
             "#", "Name", "State", "git clone",
             width = max_name,
+            state = max_state,
             dig = length,
         );
         for (index, repo) in $r.iter().enumerate() {
             eprintln!(
-                " {0: ^dig$} | {1: <width$} | {2: <7} | {3}",
+                " {0: ^dig$} | {1: <width$} | {2: <state$} | {3}",
                 index + 1, repo.full_name, repo.state, repo.clone_url,
                 width = max_name,
+                state = max_state,
                 dig = length,
             );
         }
-
     };
 }
