c8250227358acb0396a666d2caf31d0a90aaeb77
feraxhp <feraxhp+gh@gmail.com>
2025-12-02 08:05:43 -0500

    *! (#38,1) pconf resolution

--- src/commands/validations/pconf.rs
+++ src/commands/validations/pconf.rs
@@ -14,16 +14,18 @@ impl Validations for Pconf {
             return Err(cformat!("\n* No <m,i>pconfs</> configured, please configure some")) 
         }
         
-        let posible_values: Vec<String> = usettings.pconfs.iter().map( | p |  cformat!("<g>{}</>", p.name.clone())).collect();
+        let posible_values: String = usettings.pconfs.iter()
+            .flat_map( | p |  vec![cformat!("<g>{}</>, ", p.name.clone())])
+            .collect();
         
         match usettings.get_pconf_by_name(value) {
             Some(e) => Ok(e),
             
-            None if value.eq("*") && !usettings.default.is_empty() 
+            None if value.eq(".") && !usettings.default.is_empty() 
             => Ok(usettings.get_default_pconf().unwrap()),
             
             None 
-            => Err(cformat!("\n* Posible values are {:?} and <m>*</> for default", posible_values)),
+            => Err(cformat!("\n* Posible values are [{posible_values}] and '<m>.</>' for default")),
         }
     }
     
