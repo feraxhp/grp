b4b421b51e4f6d61ef6217e688e22d45f1345627
feraxhp <feraxhp+gh@gmail.com>
2025-12-06 17:31:49 -0500

    *! None::Animation

--- src/girep/animation.rs
+++ src/girep/animation.rs
@@ -9,11 +9,13 @@ pub(crate) trait Animation {
 
 pub struct None;
 
+impl None { fn new() -> Box<None> { Box::new(None) } }
+
 #[allow(unused_variables)]
 impl Animation for None {
-    fn new<T: Into<String>>(message: T) -> Box<Self> { unreachable!() }
-    fn finish_with_error<T: Into<String>>(&self, message: T) { unreachable!() }
-    fn finish_with_warning<T: Into<String>>(&self, message: T) { unreachable!() }
-    fn finish_with_success<T: Into<String>>(&self, message: T) { unreachable!() }
-    fn change_message<T: Into<String>>(&self, message: T) { unreachable!() }
+    fn new<T: Into<String>>(message: T) -> Box<Self> { Self::new() }
+    fn finish_with_error<T: Into<String>>(&self, message: T) { }
+    fn finish_with_warning<T: Into<String>>(&self, message: T) {  }
+    fn finish_with_success<T: Into<String>>(&self, message: T) {  }
+    fn change_message<T: Into<String>>(&self, message: T) {  }
 }
